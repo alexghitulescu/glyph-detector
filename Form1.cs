631328b5a191e6ac84ef64485287102bb3efcafb
Alexandru Ghitulescu <pszag@nottingham.ac.uk>
2015-07-29 15:34:06 +0100

    added code

---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace GlyphTest
+{
+    public partial class Form1 : Form
+    {
+        Processor processor;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            processor = new Processor();
+            processor.PostAction += new PostProcess(onProcess);
+
+            initVideo();
+        }
+
+        private void initVideo()
+        {
+            // enumerate video devices
+            FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            // create video source
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            // set NewFrame event handler
+            videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            // start the video source
+            videoSource.Start();
+        }
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // get new frame
+            Bitmap bitmap = eventArgs.Frame;
+
+            Bitmap imediateDraw = new Bitmap(bitmap, new Size(pictureBox2.Width, pictureBox2.Height));
+
+            pictureBox2.Invoke((Action)delegate()
+            {
+                if (pictureBox2.Image != null)
+                {
+                    pictureBox2.Image.Dispose();
+                }
+
+                pictureBox2.Image = imediateDraw;
+            });
+
+            Bitmap clone = new Bitmap(bitmap.Width, bitmap.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            using (Graphics gr = Graphics.FromImage(clone))
+            {
+                gr.DrawImage(bitmap, new Point(0, 0));
+            }
+
+            processor.Image = clone;
+
+            bitmap.Dispose();
+        }
+
+        private void onProcess(Bitmap image)
+        {
+            pictureBox1.Invoke((Action)delegate()
+            {
+                if (pictureBox1.Image != null)
+                {
+                    pictureBox1.Image.Dispose();
+                }
+
+                pictureBox1.Image = (Bitmap) image.Clone();
+            });
+        }
+    }
+}
---
+++ Processor.cs
@@ -0,0 +1,164 @@
+﻿using AForge;
+using AForge.Imaging.Filters;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision.GlyphRecognition;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace GlyphTest
+{
+    public delegate void PostProcess(Bitmap bitmap);
+
+    class Processor
+    {
+        private GlyphDatabase database;
+        private GlyphRecognizer recogniser;
+
+        private BaseResizeFilter resizeFilter;
+        private Sharpen sharpenFilter;
+
+        private Object lockObj = new Object();
+        private Bitmap image;
+
+        private bool running = true;
+        private AutoResetEvent autoEvent;
+
+        public Bitmap Image
+        {
+            private get
+            {
+                lock (lockObj)
+                {
+                    return image;
+                }
+            }
+
+            set
+            {
+                lock (lockObj)
+                {
+                    image = value;
+                }
+
+                autoEvent.Set();
+            }
+        }
+
+        public PostProcess PostAction;
+
+        public Processor()
+        {
+            initGlyphs();
+
+            // resizeFilter = new ResizeNearestNeighbor(1920, 1080);
+            // resizeFilter = new ResizeBicubic(1920, 1080);
+            resizeFilter = new ResizeBilinear(1920, 1080);
+            sharpenFilter = new Sharpen();
+
+            autoEvent = new AutoResetEvent(false);
+
+            Thread thread = new Thread(process);
+            thread.Start();
+        }
+
+        private void initGlyphs()
+        {
+            database = new GlyphDatabase(5);
+
+            Glyph glyph1 = new Glyph("1    ", new byte[,] { {0, 0, 0, 0, 0},
+                                                            {0, 0, 1, 1, 0},
+                                                            {0, 1, 0, 1, 0},
+                                                            {0, 0, 1, 0, 0},
+                                                            {0, 0, 0, 0, 0} });
+
+            Glyph glyph2 = new Glyph("2    ", new byte[,] { {0, 0, 0, 0, 0},
+                                                            {0, 1, 0, 1, 0},
+                                                            {0, 1, 1, 1, 0},
+                                                            {0, 0, 1, 0, 0},
+                                                            {0, 0, 0, 0, 0} });
+            database.Add(glyph1);
+            database.Add(glyph2);
+
+            recogniser = new GlyphRecognizer(database);
+        }
+
+        private void process()
+        {
+            while (running)
+            {
+                while (Image == null)
+                {
+                    if (!running)
+                    {
+                        return;
+                    }
+                    autoEvent.WaitOne();
+                }
+
+                Bitmap tempImg;
+                lock (lockObj)
+                {
+                    tempImg = Image;
+                    Image = null;
+                }
+
+                try
+                {
+                    var temp = tempImg.RawFormat;
+                }
+                catch(Exception)
+                {
+                    continue;
+                }
+
+                Console.WriteLine("width: " + tempImg.Width + " ; height: " + tempImg.Height);
+
+                // Bitmap bigger = new Bitmap(tempImg, new Size(tempImg.Width, tempImg.Height));
+                // Bitmap bigger = new Bitmap(tempImg, new Size(1920, 1080));
+                Bitmap bigger = resizeFilter.Apply(tempImg);
+                bigger = sharpenFilter.Apply(bigger);
+
+                List<ExtractedGlyphData> glyphDataList = recogniser.FindGlyphs(bigger);
+
+                using (Graphics g = Graphics.FromImage(bigger))
+                {
+                    foreach (ExtractedGlyphData glyphData in glyphDataList)
+                    {
+                        List<IntPoint> glyphPoints = (glyphData.RecognizedGlyph == null) ? glyphData.Quadrilateral : glyphData.RecognizedQuadrilateral;
+
+                        Pen pen = new Pen(Color.Red, 3);
+
+                        // highlight border
+                        g.DrawPolygon(pen, ToPointsArray(glyphPoints));
+                    }
+                }
+
+                if (PostAction != null)
+                {
+                    PostAction(bigger);
+                }
+            }
+        }
+
+        #region Helper methods
+        // Convert list of AForge.NET framework's points to array of .NET's points
+        private static System.Drawing.Point[] ToPointsArray(List<IntPoint> points)
+        {
+            int count = points.Count;
+            System.Drawing.Point[] pointsArray = new System.Drawing.Point[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                pointsArray[i] = new System.Drawing.Point(points[i].X, points[i].Y);
+            }
+
+            return pointsArray;
+        }
+        #endregion
+    }
+}
